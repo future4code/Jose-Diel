bac0baa2748514e12f0479c56008088f59bd3ecd
Julian Diel <52138612+JulianDiel@users.noreply.github.com>
2021-09-10 21:40:29 -0300

    esta incompleto o exercicio, faltando apenas o exercicio 03

---
+++ quinzena1/operadores/index.js
@@ -0,0 +1,97 @@
+/*const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado) // false // se tiver ao menos 01 falso, todos serao falso
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) // false // se tiver ao menos 01 falso, todos serao falso
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado) // true (sempre que tiver ! fica ao contrario)
+
+console.log("d. ", typeof resultado) // boolean // true ou false são booleanos */
+
+
+/*Corrigindo codigo do amigo
+
+let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) // ele nao pediu adiçao no codigo, ficara correto na demonstração abaixo */
+
+/*let primeiroNumero = Number (prompt("Digite um numero!"))
+let segundoNumero = Number (prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) // Faltou declarar Number na Variavel, caso nao declare o computador une achando que é String */
+
+/*Comparando idade x idade amigo
+
+const idadeUsuario = Number (prompt('Qual sua idade?'))
+const idadeAmigo = Number (prompt('Qual a idade do seu melhor amigo(a)?'))
+
+let difIdade = (idadeUsuario >= idadeAmigo)
+let difIdade2 = (idadeUsuario % idadeAmigo)
+
+console.log('Sua é idade é maior que de seu amigo(a)?', difIdade)
+console.log('Sua é a diferença de idade entre vocês é de', difIdade2, ('anos.')) */
+
+/*exercicio 2
+
+let nPar = Number(prompt("Digite um numero par"))
+
+console.log("O resto da divisão do seu numero digita:", nPar, "é: ", nPar % 2) */
+
+/* idade do usuario
+let idadeAnos = Number(prompt('Digite sua idade: '))
+
+console.log(idadeAnos * 12, ('Sua idade em meses'))
+console.log(idadeAnos * 365, ('sua idade em dias'))
+console.log(idadeAnos * 24, ('sua idade em horas'))*/
+
+// Exercicio 4
+
+/*const primeiroNumero = Number(prompt('digite um numero: '))
+const segundoNumero = Number(prompt('digite outro numero: '))
+const resto = (primeiroNumero / segundoNumero)
+
+console.log(primeiroNumero > segundoNumero)
+console.log(primeiroNumero === segundoNumero)
+console.log(primeiroNumero === segundoNumero)
+console.log(segundoNumero === primeiroNumero) */
+// com muito sacrificio e quebrando a cabeça, consegui resolver esse problema
+
+//desafio 01 
+/* não consegui fazer esse exercicio
+
+let grauF = 
+let grauC = (9/5) + 32
+let grauK = (-32)*(5/9) + 273.15
+
+
+console.log()
+console.log(grauF / grauC)
+console.log()
+console.log()
+*/
+
+//Desafio 02
+
+/*const qW = 0.05
+const qH = 60
+const hora = 280
+let resDesconto = (1,15)
+let total = qW * qH
+let  = 
+
+
+console.log(total, 'reais à pagar')
+console.log(280 * qW, 'reais à pagar')
+console.log(resDesconto - total, 'reais à paga com 15% de desconto')*/
+
+
