430a76f6aced71f8ff59d08c51c1230ad76b3e70
Julian Diel <52138612+JulianDiel@users.noreply.github.com>
2021-09-19 03:54:59 -0300

    estou mandando template, espero que vá o correto

---
+++ quinzena2/funcoes/index.js
@@ -0,0 +1,27 @@
+//01
+
+/*function minhaFuncao(variavel) {
+	return variavel * 5
+}
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10))
+//a) 10, 50
+//b) nao aconteceria nada*/
+
+/////////////////////////////////
+
+//02
+/*let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)*/
+ //a) é um codigo includes de cenoura, se digitarmos cenoura no prompt ele da true, qualquer outra string da false
+
+//b) qualquer texto que tenha a string cenoura será contabilizada como TRUE
+
+
