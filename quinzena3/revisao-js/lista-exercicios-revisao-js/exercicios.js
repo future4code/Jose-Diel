d5e604a0771e57068b9628dc37fffb2c1b942342
Julian Diel <52138612+JulianDiel@users.noreply.github.com>
2021-10-05 19:17:49 -0300

    concluido 05 exercicios, consegui pesquizando na internet, mas ainda nao tenho muito intendimento de como eu fiz, mas nao copiei de ninguem

---
+++ quinzena3/revisao-js/lista-exercicios-revisao-js/exercicios.js
@@ -0,0 +1,166 @@
+
+// EXERCÍCIO 01
+function inverteArray(array) {
+  let arrayInvertido = []
+  for(let i = array.length-1; i >=0; i--){
+    arrayInvertido.push(array[i])
+  }
+  console.log(arrayInvertido)
+  return arrayInvertido
+}
+
+// EXERCÍCIO 02
+function retornaNumerosParesElevadosADois(array) {
+  let arrayNumerosPares = [];
+  for (let i=0; i< array.length; i++){
+    if (array[i] %2 === 0){
+      arrayNumerosPares.push(array[i]**2)
+    }
+  }
+  console.log(arrayNumerosPares)
+  return arrayNumerosPares
+}
+// EXERCÍCIO 03
+function retornaNumerosPares(array) {
+  let arrayNumerosPares = [];
+  for (let i=0; i< array.length; i++){
+    if (array[i] %2 === 0){
+      arrayNumerosPares.push(array[i])
+    }
+  }
+  console.log(arrayNumerosPares)
+  return arrayNumerosPares
+}
+// EXERCÍCIO 04
+function retornaMaiorNumero(array) {
+  let numeros = -Infinity, i = 0, n = array.length;
+
+  for (; i != n; ++i) {
+      if (array[i] > numeros) {
+          numeros = array[i];
+      }
+  }
+
+  return numeros;
+}
+
+// EXERCÍCIO 05
+function retornaQuantidadeElementos(array) {
+  
+  const quantidadeElementos = []
+  for (let i=0; i< array.length; i++){
+    if (array[i] > quantidadeElementos){
+      quantidadeElementos === array[i];
+    }
+  }
+  return array.length
+}
+
+
+// EXERCÍCIO 06
+function retornaExpressoesBooleanas() {
+  
+}
+
+/*// EXERCÍCIO 07
+function retornaNNumerosPares(n) {
+
+}
+
+// EXERCÍCIO 08
+function checaTriangulo(a, b, c) {
+  // return 'Escaleno'
+  // return 'Equilátero'
+  // return 'Isósceles'
+}
+
+// EXERCÍCIO 09
+function comparaDoisNumeros(num1, num2) {
+  // Formato do objeto a ser retornado:
+  // {
+  //   maiorNumero: X,
+  //   maiorDivisivelPorMenor: Y,
+  //   diferenca: Z
+  // }
+}
+
+// EXERCÍCIO 10
+function segundoMaiorEMenor(array) {
+
+}
+
+// EXERCÍCIO 11
+function ordenaArray(array) {
+
+}
+
+// EXERCÍCIO 12
+function filmeFavorito() {
+
+}
+
+// EXERCÍCIO 13
+function imprimeChamada() {
+  // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+}
+
+// EXERCÍCIO 14
+function criaRetangulo(lado1, lado2) {
+
+}
+
+// EXERCÍCIO 15
+function anonimizaPessoa(pessoa) {
+
+}
+
+// EXERCÍCIO 16A
+function maioresDe18(arrayDePessoas) {
+
+}
+
+// EXERCÍCIO 16B
+function menoresDe18(arrayDePessoas) {
+
+}
+
+// EXERCÍCIO 17A
+function multiplicaArrayPor2(array) {
+
+}
+
+// EXERCÍCIO 17B
+function multiplicaArrayPor2S(array) {
+
+}
+
+// EXERCÍCIO 17C
+function verificaParidade(array) {
+
+}
+
+// EXERCÍCIO 18A
+function retornaPessoasAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 18B
+function retornaPessoasNaoAutorizadas(pessoas) {
+
+}
+
+// EXERCÍCIO 19A
+function ordenaPorNome(consultasNome) {
+
+}
+
+// EXERCÍCIO 19B
+function ordenaPorData(consultasData) {
+
+}
+
+// EXERCÍCIO 20
+function calculaSaldo(contas) {
+
+}
+*/
